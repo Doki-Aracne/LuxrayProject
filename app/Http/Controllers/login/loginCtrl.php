d64ef722c8c0fce84e59506ee1e6b00e1de20837
Doki-Aracne <daniel.meraz@iteesa.edu.hn>
2024-09-13 11:28:20 -0600

    feat: nuevo controlador

--- app/Http/Controllers/login/loginCtrl.php
+++ app/Http/Controllers/login/loginCtrl.php
@@ -9,6 +9,6 @@ use Illuminate\Http\Request;
 class loginCtrl extends Controller
 {
     public function mostrarDatos(){
-        return view('registro.frmRegistro')
+        return view('registro.frmRegistro');
     }
 }
